3bac1931da30336c2c277a78b3a4a7bab548e346
sandrocardona <sandrocardona41@gmail.com>
2023-11-20 11:15:56 +0100

    20  NOV

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { Button } from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        {' '}
+        <Button color="success">
+          Inglés
+        </Button>
+        {' '}
+        <Button color="primary">
+          Alemán
+        </Button>
+        {' '}
+        <Button color='danger'>
+          Sueco
+        </Button>
+        {' '}
+        <Button color="success">
+        Español
+        </Button>
+      </div>
+    );
+  }
 }
 
 export default App;
