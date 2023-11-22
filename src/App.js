159065f70fb1f498e08483e1c38c85e92c4c90a7
sandrocardona <sandrocardona41@gmail.com>
2023-11-22 12:46:58 +0100

    22 NOV

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import { Button } from 'reactstrap';
+import { Button, Spinner } from 'reactstrap';
 
 function Boton(props){
   return (
-    <Button color={props.color} onClick={props.cambia} >
-      Pulsa para cambiar el color
+    <Button color={props.color} onClick={props.cambia}>
+      {props.nombre}
     </Button>
+  )
+}
+
+const Circulo = (props) =>{
+  return(
+    <Spinner color={props.color}>
+    </Spinner>
   )
 }
 
@@ -14,22 +21,33 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      color:"danger",
+      letrero:"",
+      color:""
     }
   }
 
-  cambia(){
-    if(this.state.color === "danger"){
-      this.setState({color:"success"})
-    }else{
-      this.setState({color:"danger"})
-    }
+  cambiaIngles(){
+    this.setState({letrero:"Hello, how are you today?"});
+    this.setState({color:"secondary"})
+  }
+
+  cambiaAleman(){
+    this.setState({letrero:"Hallo, wie geht es dir?"});
+    this.setState({color:"danger"})
+  }
+
+  cambiaEspañol(){
+    this.setState({letrero:"Hola, ¿qué tal estas?"});
+    this.setState({color:"primary"})
   }
 
   render() {
     return (
       <div className="App">
-        <Boton color={this.state.color} cambia={()=>this.cambia()}/>
+        <h1><Circulo color={this.state.color} type="grow"/>color:{this.state.color}</h1>
+        <Boton color="secondary" nombre={"secondary"} cambia={()=>this.cambiaIngles()}/>
+        <Boton color="danger" nombre={"danger"} cambia={()=>this.cambiaAleman()}/>
+        <Boton color="primary" nombre={"primary"} cambia={()=>this.cambiaEspañol()}/>
       </div>
     );
   }
